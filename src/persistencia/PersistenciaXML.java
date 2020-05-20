48e92b547be7e1b627b74dbd44f8128ffd6c3670
Luciano <luchyfran97@gmail.com>
2020-05-20 17:33:46 -0300

    Persistencia BIN completa

---
+++ src/persistencia/PersistenciaXML.java
@@ -0,0 +1,32 @@
+package persistencia;
+
+import java.io.IOException;
+
+public class PersistenciaXML implements IPersistencia {
+
+	@Override
+	public void abrirInput(String nombre) throws IOException {
+	}
+
+	@Override
+	public void abrirOutput(String nombre) throws IOException {
+	}
+
+	@Override
+	public void cerrarInput() throws IOException {
+	}
+
+	@Override
+	public void cerrarOutput() throws IOException {
+	}
+
+	@Override
+	public void escribir(Object objeto) throws IOException {
+	}
+
+	@Override
+	public Object leer() throws IOException, ClassNotFoundException {
+		return null;
+	}
+
+}
